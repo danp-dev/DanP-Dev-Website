a2d7230410a12b887a27dd5112539b4dffab5074
Dan <github@danp.dev>
2021-01-13 21:12:53 +1030

    Initial

--- src/components/header.js
+++ src/components/header.js
@@ -2,33 +2,45 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import logo from "../images/Dan-P-Dev-Logo.svg"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+<nav className="navbar" role="navigation" aria-label="main navigation">
+  <container className="container">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="https://danp.dev">
+      <img src={logo}/>
+    </a>
+
+    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+</div>
+</div>
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a className="button is-primary">
+            <strong>Say Hello!
+            </strong>
+          </a>
+          <a className="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
     </div>
-  </header>
+  
+  </container>
+</nav>
+</header>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,36 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import avatar from "../images/DanP-Avatar.svg"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    
+    <section class="hero is-white has-text-centered">
+      <div class="hero-body">
+      <div className="container">
+          <div className="columns is-centered">
+            <div className="column">
+              <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">Designer, Front-end Developer Mentor</h1>
+              <h2 className="subtitle is-size-4-desktop">I design and code beautifully simple things, and I love what I do.</h2><img class="avatar" src={avatar}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="section is-medium has-background-primary has-text-centered has-text-white">
+      <div className="container ">
+        <div className="columns is-centered">
+          <div className="column is-three-fifths">
+            <h1 className="title is-spaced is-size-3-desktop is-size-4-mobile has-text-white">Hi, I’m Matt. Nice to meet you.</h1>
+            <h2 className="subtitle is-size-5-desktop has-text-white">Since beginning my journey as a freelance designer nearly 10 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
